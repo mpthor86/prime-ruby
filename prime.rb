d089fdaf34995a7d6301029ce48b489c65850f5c
michael <mpthornton86@gmail.com>
2020-10-26 10:15:27 -0600

    commit tree fa0f2a2e51bc5a58dab99c9794c0067fa004da2d

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(x)
+    if x < 0 or x == 0 or x == 1
+        false
+    else 
+        (2..x-1).to_a.all? do |number|
+            x % number != 0
+        end
+    end
+end
